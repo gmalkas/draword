48ac8ede93e308c675ab71bc3eb67ecfc496b914
Gabriel Malkas <gabriel.malkas@gmail.com>
2013-02-27 17:13:20 +0100

    Add the notion of turns count to Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,16 +1,20 @@
+require 'ostruct'
+
 class Room
   
   class HasOngoingGameException < RuntimeError; end
   class CannotJoinFullRoomException < RuntimeError; end
 
-  attr_reader :capacity, :current_game, :name, :players
+  attr_reader :capacity, :current_game, :name, :players, :turns_count
 
   DEFAULT_CAPACITY = 10
+  DEFAULT_TURNS_COUNT = 10
 
-  def initialize(name, capacity=DEFAULT_CAPACITY)
+  def initialize(name, capacity=DEFAULT_CAPACITY, turns_count=DEFAULT_TURNS_COUNT)
     @capacity = capacity
     @name = name
     @players = []
+    @turns_count = turns_count
   end
 
   def full?
@@ -23,7 +27,7 @@ class Room
 
   def new_game
     raise HasOngoingGameException if has_current_game?
-    @current_game = ""
+    @current_game = OpenStruct.new turns_count: @turns_count
   end
 
   def new_player(player)
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -25,6 +25,15 @@ describe Room do
     my_room.capacity.should == 15
   end
 
+  it "has a default turns count" do
+    room.turns_count.should == 10
+  end
+
+  it "can have a custom turns count" do
+    my_room = Room.new "My room", nil, 15
+    my_room.turns_count.should == 15
+  end
+
   it "can be full" do
     my_room = Room.new "My room", 1
     my_room.new_player "Gabriel"
@@ -36,8 +45,7 @@ describe Room do
       let(:room) { Room.new "My room", 0 }
 
       it "does not accept new player" do
-        my_room = Room.new "My room", 0
-        expect { my_room.new_player("Gabriel") }.to raise_error(Room::CannotJoinFullRoomException)
+        expect { room.new_player("Gabriel") }.to raise_error(Room::CannotJoinFullRoomException)
       end
 
     end
@@ -51,13 +59,19 @@ describe Room do
 
   end
 
-
   describe "#new_game" do
     context "when there is not a current game" do
-      it "can create a new game" do
+      it "creates a new game" do
         room.new_game
         room.has_current_game?.should be_true
       end
+
+      it "creates new games with custom turns count" do
+        my_room = Room.new "My room", 10, 15
+        game = my_room.new_game
+        game.turns_count.should == 15
+      end
+
     end
 
     context "when there is a current game" do
